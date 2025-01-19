f319316bdfb3a56ae9311bf337647bba1a000840
M Rakib Ahmed <ahmedmrakib24@gmail.com>
2025-01-19 08:38:14 +0600

    new regester added

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,9 +1,18 @@
 
 const Register = () => {
+    const handleRegister = (even) =>{
+        even.preventDefault();
+        const email = even.target.email.value;
+        console.log(email);
+        const password = even.target.password.value;
+        console.log(password);
+        
+
+    }
     return (
         <div className="space-y-5">
           <h2 className="text-4xl font-bold">Register </h2>  
-          <form className="space-y-10">
+          <form className="space-y-10" onSubmit={handleRegister}>
                 <label className="input input-bordered flex items-center gap-2">
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
@@ -15,7 +24,7 @@ const Register = () => {
                         <path
                             d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
                     </svg>
-                    <input type="text" className="grow" placeholder="Email" />
+                    <input type="email"name="email" className="grow" placeholder="Email" />
                 </label> 
                 <label className="input input-bordered flex items-center gap-2">
                     <svg
@@ -28,8 +37,9 @@ const Register = () => {
                             d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
                             clipRule="evenodd" />
                     </svg>
-                    <input type="password" className="grow" value="password" />
+                    <input type="password" name="password" className="grow" placeholder="password"/>
                 </label>
+                <button className="btn btn-wide btn-error text-xl text-white uppercase">Login</button>
           </form>
         </div>
     );
---
+++ src/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDzNtNnBaNn7rJdDrV1fWEtO2PAEzrFzrQ",
+  authDomain: "email-password-aurth-4662d.firebaseapp.com",
+  projectId: "email-password-aurth-4662d",
+  storageBucket: "email-password-aurth-4662d.firebasestorage.app",
+  messagingSenderId: "209464555816",
+  appId: "1:209464555816:web:1111d16a43b49ecfae381d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app
